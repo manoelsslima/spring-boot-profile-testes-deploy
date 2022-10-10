2d4ffd37a7f297a314c2b6454b3c68e33827c835
Manoel Lima <manoel.lima@tjac.jus.br>
2022-10-10 03:31:24 -0400

    Testing controller

--- forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ forum/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -4,6 +4,7 @@ import br.com.alura.forum.config.security.TokenAPIService;
 import br.com.alura.forum.controller.dto.TokenDTO;
 import br.com.alura.forum.controller.form.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,6 +24,7 @@ import javax.validation.Valid;
  */
 @RestController
 @RequestMapping("/auth")
+@Profile(value = {"prod", "test"}) // loaded in both profiles
 public class AutenticacaoController {
 
     // dispara o processo de autenticação
